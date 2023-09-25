4839d31339e5588f80de53a6c2bfbf993c5a02e1
greenluca <luca.marchesini@greenly.earth>
2023-09-25 16:35:28 +0200

    simplified

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -3,61 +3,45 @@
 // It cannot access the DOM, it can access the chrome.tabs API.
 // The purpose of this worker is to give content-script.js access to the chrome.tabs API.
 
-// These are messages that content script can send to service worker.
-type NavigateToTab = {
-  type: "NAVIGATE_TO_TAB";
-  tabId: number;
-  url: string;
-};
+const GREENLY_SAAS_URL = 'carbon.greenly.earth'
 
-type GetOpenRetoolTabs = {
-  type: "GET_OPEN_RETOOL_TABS";
-};
+async function getOpenGreenlyTabs():Promise<chrome.tabs.Tab[]> {
+  const allTabs = await chrome.tabs.query({ url: "<all_urls>" });
+  return allTabs.filter((tab) => tab.url?.includes(GREENLY_SAAS_URL));
+}
 
-// https://developer.chrome.com/docs/extensions/mv3/messaging/#connect
-chrome.runtime.onConnect.addListener(function (port) {
-  console.assert(port.name === "main-background-channel");
-
-  port.onMessage.addListener(async function (
-    msg: NavigateToTab | GetOpenRetoolTabs
-  ) {
-    console.log("Retool Quickopen service worked received a message:");
-    console.log(msg);
-
-    // Content script is asking for a list of open Retool tabs.
-    if (msg.type === "GET_OPEN_RETOOL_TABS") {
-      let tabs = await chrome.tabs.query({ url: "<all_urls>" });
-      console.log(tabs);
-      tabs = tabs.filter((tab) => tab.url?.includes("retool"));
-      const payload: UpdateDOMWithOpenRetoolTabs = {
-        type: "UPDATE_DOM_WITH_OPEN_RETOOL_TABS",
-        openRetoolTabs: JSON.stringify(tabs),
-      };
-      port.postMessage(payload);
-    }
-
-    // Content script is asking to navigate to a different tab.
-    // Verify the tab still exists.
-    if (msg.type === "NAVIGATE_TO_TAB") {
-      let tab;
-      try {
-        tab = await chrome.tabs.get(msg.tabId);
-      } catch (e) {
-        console.log(e);
-      }
-
-      if (tab) {
-        // Tab exists, navigate to it.
-        chrome.tabs.update(msg.tabId, {
-          url: msg.url,
-        });
-        chrome.tabs.update(msg.tabId, {
-          active: true,
-        });
-      } else {
-        // Tab does not exist, create it.
-        chrome.tabs.create({ url: msg.url });
-      }
-    }
+function findTabByURL(openTabs: chrome.tabs.Tab[], url: URL) {
+  return openTabs.find((tab) => {
+    const tabUrl = new URL(tab.url ? tab.url : '');
+    return tabUrl.hostname === url.hostname && tabUrl.pathname === url.pathname;
   });
-});
+}
+
+async function navigateToTab(tab: chrome.tabs.Tab): Promise<void> {
+  if (!tab.id) {
+    console.error(`Unable to open tab with no id`)
+    return
+  }
+  try {
+    await chrome.tabs.get(tab.id);
+  } catch (e) {
+    console.error(e);
+    return
+  }
+
+  console.log('RetoolQuickOpen: tab exists, navigate to it.')
+  chrome.tabs.update(tab.id, {
+    active: true,
+  });
+}
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (changeInfo.url) {
+    const openTabs = await getOpenGreenlyTabs()
+    const targetTab = findTabByURL(openTabs, new URL(changeInfo.url))
+
+    if (targetTab) {
+      await navigateToTab(targetTab)
+    }
+  }
+})
